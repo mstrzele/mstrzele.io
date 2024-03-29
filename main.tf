9aae3c77e98a986272d20414baf182b41edb2c24
Maciej Strzelecki <mstrzele@users.noreply.github.com>
2024-03-29 22:12:03 +0100

    feat(terraform): mstrzele.github.io

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4"
     }
+
+    github = {
+      source  = "integrations/github"
+      version = "~> 6"
+    }
   }
 
   cloud {
@@ -17,15 +22,40 @@ terraform {
   }
 }
 
+provider "github" {
+  owner = "mstrzele"
+
+  app_auth {
+    id              = var.app_id
+    installation_id = var.app_installation_id
+    pem_file        = var.app_pem_file
+  }
+}
+
 data "cloudflare_zone" "mstrzele_io" {
   name = "mstrzele.io"
+}
+
+resource "github_repository" "mstrzele_github_io" {
+  name = "mstrzele.github.io"
+
+  pages {
+    build_type = "workflow"
+    cname      = data.cloudflare_zone.mstrzele_io.name
+  }
+}
+
+resource "github_repository_file" "cname" {
+  repository = github_repository.mstrzele_github_io.name
+  file       = "CNAME"
+  content    = data.cloudflare_zone.mstrzele_io.name
 }
 
 resource "cloudflare_record" "mstrzele_io" {
   zone_id = data.cloudflare_zone.mstrzele_io.id
   type    = "CNAME"
-  name    = "mstrzele.io"
-  value   = "mstrzele.github.io"
+  name    = data.cloudflare_zone.mstrzele_io.name
+  value   = github_repository.mstrzele_github_io.name
   proxied = true
 }
 
@@ -33,6 +63,6 @@ resource "cloudflare_record" "www" {
   zone_id = data.cloudflare_zone.mstrzele_io.id
   type    = "CNAME"
   name    = "www"
-  value   = "mstrzele.github.io"
+  value   = github_repository.mstrzele_github_io.name
   proxied = true
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "app_id" {
+  type = string
+}
+
+variable "app_installation_id" {
+  type = string
+}
+
+variable "app_pem_file" {
+  type = string
+}
